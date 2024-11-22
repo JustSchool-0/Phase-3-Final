6d90ab549081eddf53bfb4a86b579fff20f6fe41
Me <pdprince.lfc+github@gmail.com>
2024-11-22 09:23:35 -0500

    add clear_console function to helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,8 @@
 # lib/helpers.py
 
+from os import system, name
+
+
 def helper_1():
     print("Performing useful function#1.")
 
@@ -7,3 +10,13 @@ def helper_1():
 def exit_program():
     print("Goodbye!")
     exit()
+
+
+def clear_console():
+    # for windows
+    if name == "nt":
+        _ = system("cls")
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system("clear")
